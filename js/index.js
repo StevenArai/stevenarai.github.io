5f90a217ff1beceb0331e80746721cc83cd51703
StevenArai <84560813+StevenArai@users.noreply.github.com>
2023-12-01 14:36:53 +0800

    finished banner framework

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 
-var imgPointer=0;
 
 window.onload=function(){/*
 	var bannerHandle=document.getElementById('banner-handle');
@@ -12,17 +11,26 @@ window.onload=function(){/*
 	autoScrollTimer = setTimeout(scrollBanner,3000,'R');
 	console.log(' __  __     ______     ______  \n/\\ \\_\\ \\   /\\  __ \\   /\\__  _\\ \n\\ \\  __ \\  \\ \\  __ \\  \\/_/\\ \\/ \n \\ \\_\\ \\_\\  \\ \\_\\ \\_\\    \\ \\_\\ \n  \\/_/\\/_/   \\/_/\\/_/     \\/_/ \n                               \nDEBUG CONSOLE\tDEBUG MODE = OFF');
 };
+window.onresize=function(){
+	scrollBanner(null);
+};
 var autoScrollTimer;
 function bannerScrolling(){
 	clearTimeout(autoScrollTimer);
 	autoScrollTimer = setTimeout(scrollBanner,4500,'R');
 };
-
+var imgPointer=0;
 function scrollBanner(scrollDirection){//L or R
 	bannerHandle=document.getElementById('banner-handle');
 	var imgCount = (document.getElementById('banner-handle').childNodes.length-1)/2;
 	if(scrollDirection=='L') imgPointer--;
-	if(scrollDirection=='R') imgPointer++;
+	else if(scrollDirection=='R') imgPointer++;
+	else if(scrollDirection==null) ;
+	else {
+		imgPointer=scrollDirection-1;
+		console.log(scrollDirection-0);
+	}
+
 	//change the img
 	if(imgPointer<0) imgPointer=imgCount-1;
 	if(imgPointer>imgCount-1) imgPointer=0;
@@ -31,5 +39,5 @@ function scrollBanner(scrollDirection){//L or R
 		left: (bannerHandle.scrollWidth/imgCount+1)*imgPointer,
 		behavior: "smooth"
 	});
-//	console.log('['+imgPointer+'] '+((bannerHandle.scrollWidth/imgCount+1)*imgPointer));
+	document.getElementById('banner-control').setAttribute('style','margin-left: '+(14+84*imgPointer)+'px');
 };
