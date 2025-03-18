deacd3ec1ace373a9f096c812c8f2a57fbc8c027
StevenArai <84560813+StevenArai@users.noreply.github.com>
2025-03-19 01:02:38 +0800

    upload manga

---
+++ manga/同人漫画_クイック賄派_家_おうち [加帕里图书馆 - 兽娘动物园中文维基]_files/img.php
@@ -0,0 +1,6 @@
+<br />
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function imagecreatetruecolor() in /var/www/html/lib/plugins/captcha/helper.php:365
+Stack trace:
+#0 /var/www/html/lib/plugins/captcha/img.php(20): helper_plugin_captcha-&gt;_imageCAPTCHA('CEIWR')
+#1 {main}
+  thrown in <b>/var/www/html/lib/plugins/captcha/helper.php</b> on line <b>365</b><br />
